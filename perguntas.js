c762add7859ada77a2e4fffb2a95e7b0e2ef0bc2
Lucas <00001109866550sp@al.educacao.sp.gov.br>
2024-11-22 15:58:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'The Last Of Us', 
+    'Qual tema do jogo?',
+    'Açao e apocalpse'
+)
+
+criaCartao(
+    'The Last Of Us', 
+    'Quem é Joel?',
+    'Joel era um contrabandista, ate receber a missao de levar Ellie para um grupo que estudaria ela para chegar a uma cura para o virus'
+)
+
+criaCartao(
+    'The Last Of Us', 
+    'Quem é Ellie?',
+    'Ellie é a protagonista do jogo, ela é a unica pessoa imune ao virus e a chave para a cura '
+)
+
+criaCartao(
+    'The Last Of Us',
+    'Qual o nome do grupo que Joel precisa entregar Ellie?',
+    'Vagalumes'
+)
+
+criaCartao(
+    'The Last Of Us',
+    'Qual frase Joel fala depois de entregar uma arma para Ellie?',
+    'Faça os tiros valerem a pena'
+)
+
+criaCartao(
+    'The Last Of Us',
+    'Por que Joel não deixou usarem Ellie para obter a cura?',
+    'Joel descobriu que Ellie passaria por uma cirugia no cerebro e isso levaria a sua morte, então ele não deixou que a cirurgia fosse realizada e tirou Ellie do hospital'
+)
+
+criaCartao(
+    'The Last Of Us',
+    'Quem é Tommy?',
+    'Tommy é o irmão mais novo de Joel, e dono de uma cidade, cidade qual Joel e Ellie vão morar depois que Ellie foi salva dos vagalumes'
+)
+
+criaCartao(
+    'The Last Of Us',
+    'Por que Abby matou Joel?',
+    'Quando Joel estava tirando Ellie do hospital ele matou varias pessoas, e entre elas o pai da Abby, então Abby matou ele por vingança'
+)
+
+criaCartao(
+    'The Last Of Us',
+    'Ellie se vinga da Abby no final do jogo?',
+    'No final ela resolve deixar Abby ir e seguir a vida '
+)
